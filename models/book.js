4829fe5a7906bae1b02a679fa3a34cf78192c816
Thomas Basham <Bashamtg@gmail.com>
2022-04-12 16:42:20 -0700

    updated models

--- models/book.js
+++ models/book.js
@@ -8,7 +8,9 @@ const bookSchema = new Schema ({
   title: {type: String, require: true},
   description: {type: String, require: true},
   status: {type: Boolean, require: true},
-  email: {type: String, require: true}
+  email: {type: String, require: true},
+  author: {type: String, require: true},
+  canonicalVolumeLink: {type: String, require: true}
 
 });
 
